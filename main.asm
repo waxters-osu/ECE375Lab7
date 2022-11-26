8f5efc46c75e606565c03fb6785024eed35bd89d
IsaacGuzman2 <115694210+IsaacGuzman2@users.noreply.github.com>
2022-11-26 12:02:23 -0800

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,12 +1,104 @@
-;
-; Lab7.asm
-;
-; Created: 11/16/2022 2:24:24 PM
-; Author : Isaac
-;
+
+;***********************************************************
+;*
+;*	This is the TRANSMIT skeleton file for Lab 7 of ECE 375
+;*
+;*  	Rock Paper Scissors
+;* 	Requirement:
+;* 	1. USART1 communication
+;* 	2. Timer/counter1 Normal mode to create a 1.5-sec delay
+;***********************************************************
+;*
+;*	 Author: Gerardo Guzman
+;*	   Date: 11/16/2022
+;*
+;***********************************************************
+
+.include "m32U4def.inc"         ; Include definition file
+
+;***********************************************************
+;*  Internal Register Definitions and Constants
+;***********************************************************
+.def    mpr = r16               ; Multi-Purpose Register
+
+; Use this signal code between two boards for their game ready
+.equ    SendReady = 0b11111111
+
+;***********************************************************
+;*  Start of Code Segment
+;***********************************************************
+.cseg                           ; Beginning of code segment
+
+;***********************************************************
+;*  Interrupt Vectors
+;***********************************************************
+.org    $0000                   ; Beginning of IVs
+	    rjmp    INIT            	; Reset interrupt
 
 
-; Replace with your application code
-start:
-    inc r16
-    rjmp start
+.org    $0056                   ; End of Interrupt Vectors
+
+;***********************************************************
+;*  Program Initialization
+;***********************************************************
+INIT:
+	;Stack Pointer (VERY IMPORTANT!!!!)
+	ldi		mpr, high(RAMEND)
+	out		SPH, mpr
+
+	ldi		mpr, low(RAMEND)
+	out		SPL, mpr
+
+	;I/O Ports
+
+	;USART1
+		;Set baudrate at 2400bps
+	ldi		mpr, high(207)
+	sts		UBRR1H,mpr
+
+	ldi		mpr, low(207)
+	sts		UBRR1L,mpr
+		;Enable receiver and transmitter
+	ldi		mpr, (1<<RXEN1)|(1<<TXEN1)|(1<<RXCIE1)|(0<<UCSZ12)
+	sts		UCSR1B, mpr
+
+		;Set frame format: 8 data bits, 2 stop bits
+	ldi		mpr, (0<<UMSEL11)|(0<<UMSEL10)|(0<<UPM11)|(0<<UPM10)|(1<<USBS1)|(1<<UCSZ11)|(1<<UCSZ10)|(0<<UCPOL1)
+	sts		UCSR1C, mpr
+	;TIMER/COUNTER1
+		;Set Normal mode
+
+
+	;Other
+
+
+;***********************************************************
+;*  Main Program
+;***********************************************************
+MAIN:
+
+	;TODO: ???
+
+		rjmp	MAIN
+
+;***********************************************************
+;*	Functions and Subroutines
+;***********************************************************
+
+;***********************************************************
+;*	Stored Program Data
+;***********************************************************
+
+;-----------------------------------------------------------
+; An example of storing a string. Note the labels before and
+; after the .DB directive; these can help to access the data
+;-----------------------------------------------------------
+STRING_START:
+    .DB		"Welcome!"		; Declaring data in ProgMem
+STRING_END:
+
+;***********************************************************
+;*	Additional Program Includes
+;***********************************************************
+.include "LCDDriver.asm"		; Include the LCD Driver
+
