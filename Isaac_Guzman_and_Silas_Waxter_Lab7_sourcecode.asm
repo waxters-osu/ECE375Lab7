03be3aa73cb4b87b30401f755dfd75305bea9875
Silas <waxters@oregonstate.edu>
2022-11-28 23:25:16 -0800

    wip

--- Isaac_Guzman_and_Silas_Waxter_Lab7_sourcecode.asm
+++ Isaac_Guzman_and_Silas_Waxter_Lab7_sourcecode.asm
@@ -16,16 +16,15 @@
 ; Holds the current countdown indicator. After changing this 
 ; value, the time remaining in the countdown is equal to 
 ; approximately 1.5*countdown_indicator_r seconds.
-.def countdown_indicator_r = r24
+.def countdown_indicator_r = r25
 
-; Holds the game state of the opponent. When a message is received, 
-; decode the state and update this register.
-.def opponent_state_r = r25
-.equ opponent_state_ready = 1
-.equ opponent_state_rock = 2
-.equ opponent_state_paper = 3
-.equ opponent_state_scissors = 4
-
+; Holds the game state of both players.
+.def remote_game_state_r = r23
+.def local_game_state_r = r24		; only tracks rock, paper, scissor moves
+.equ game_state_ready_bit = 4
+.equ game_state_rock_bit = 1
+.equ game_state_paper_bit = 2
+.equ game_state_scissors_bit = 3
 
 ; Describe the LEDs used for the countdown indicator. 
 .equ countdown_indicator_ddrx = DDRB
@@ -39,8 +38,8 @@
 .equ button_ddrx = DDRD
 .equ button_portx = PORTD
 .equ button_pinx = PIND
-.equ button_ready_bit = 4
-.equ button_move_bit = 7
+.equ button_ready_bit = 7
+.equ button_move_bit = 4
 
 ; <<< DELETE ME ONCE UART IS READY
 .equ button_test1_bit = 5
@@ -49,7 +48,7 @@
 
 ; used to add some latency to debounce buttons
 .macro debounce_button
-	ldi wait_count_r, 40
+	ldi wait_count_r, 15	;150ms delay
 	rcall wait
 .endmacro
 
@@ -97,6 +96,8 @@ init:
 	out button_portx, mpr
 	; <<< DELETE ME ONCE TESTING IS COMPLETE
 
+	clr local_game_state_r
+	clr remote_game_state_r
 
 	; Initialize USART1
 	;-----
@@ -113,6 +114,12 @@ main:
 	rcall welcome_state
 
 	rcall wait_state
+
+	rcall start_state
+
+	; show move choices for both players
+
+	; display winner
 
 	rcall LCDClr
 	temp:
@@ -139,7 +146,7 @@ welcome_state:
 	rcall LCDWrite
 
 	welcome_state_await_button_press:
-		sbic button_pinx, button_move_bit
+		sbic button_pinx, button_ready_bit
 		rjmp welcome_state_await_button_press
 	
 	pop mpr
@@ -164,18 +171,150 @@ wait_state:
 	; synchronize LCD with its buffer
 	rcall LCDWrite
 
-	; TODO: UART communication replace button behavior
 	; block until receive opponenet ready message
-	ldi mpr, opponent_state_ready
-	wait_state_await_opponent_ready:
-		cp opponent_state_r, mpr
+	ldi mpr, game_state_ready_bit
+	wait_state_await_remote_ready:
+		cp remote_game_state_r, mpr
+		; TODO: send ready message to remote using UART Transmitt
 		; <<< DELETE ME ONCE UART ISR IMPLEMENTED
 		sbic button_pinx, button_test1_bit
 		; <<< DELETE ME ONCE UART ISR IMPLEMENTED
-		brne wait_state_await_opponent_ready
+		brne wait_state_await_remote_ready
 	
 	pop mpr
 	ret
+
+start_state:
+	push mpr
+
+	; copy wait message to lcd buffer
+	ldi mpr, 32
+	push mpr
+	ldi mpr, low(START_STRING)
+	push mpr
+	ldi mpr, high(START_STRING)
+	push mpr
+	ldi mpr, low(lcd_buffer_address_start_line_1)
+	push mpr
+	ldi mpr, high(lcd_buffer_address_start_line_1)
+	push mpr
+	rcall copy_prog_to_data_16
+
+	; synchronize LCD with its buffer
+	rcall LCDWrite
+
+	; block until timer expires
+	start_state_await_timer_expire:
+		; continously poll button. once pressed change move state to next
+		sbic button_pinx, button_move_bit
+		rjmp start_state_finished_move_change
+
+		debounce_button
+
+		; if game state doesn't have a move set, set it to rock
+		andi local_game_state_r, (1<<game_state_rock_bit | 1<<game_state_paper_bit | 1<<game_state_scissors_bit)
+		brne start_state_next_move
+		ldi local_game_state_r, (1<<game_state_rock_bit)
+		rjmp start_state_finished_move_change
+
+		start_state_next_move:
+			sbrc local_game_state_r, game_state_rock_bit
+			ldi local_game_state_r, 1<<game_state_paper_bit
+			sbrc local_game_state_r, game_state_paper_bit
+			ldi local_game_state_r, 1<<game_state_scissors_bit
+			sbrc local_game_state_r, game_state_scissors_bit
+			ldi local_game_state_r, 1<<game_state_rock_bit
+			
+		start_state_finished_move_change:
+			rcall display_local_move
+			; <<< DELETE ME ONCE UART ISR IMPLEMENTED
+			sbic button_pinx, button_test2_bit
+			; <<< DELETE ME ONCE UART ISR IMPLEMENTED
+			rjmp start_state_await_timer_expire
+	
+	pop mpr
+	ret
+
+display_local_move:
+	push mpr
+	push XL
+	push XH
+
+	sbrs local_game_state_r, game_state_rock_bit
+	rjmp display_local_move_rock
+
+	sbrs local_game_state_r, game_state_paper_bit
+	rjmp display_local_move_paper
+
+	sbrs local_game_state_r, game_state_scissors_bit
+	rjmp display_local_move_scissors
+
+	display_local_move_rock:
+		ldi XL, low(ROCK_STRING)
+		ldi XH, high(ROCK_STRING)
+		rjmp display_local_move_display
+
+	display_local_move_paper:
+		ldi XL, low(PAPER_STRING)
+		ldi XH, high(PAPER_STRING)
+		rjmp display_local_move_display
+
+	display_local_move_scissors:
+		ldi XL, low(SCISSORS_STRING)
+		ldi XH, high(SCISSORS_STRING)
+		rjmp display_local_move_display
+
+	display_local_move_display:
+		ldi mpr, 16
+		push mpr
+		mov mpr, XL
+		push mpr
+		mov mpr, XH
+		push mpr
+		ldi mpr, low(lcd_buffer_address_start_line_2)
+		push mpr
+		ldi mpr, high(lcd_buffer_address_start_line_2)
+		push mpr
+		rcall copy_prog_to_data_16
+
+	rcall LCDWrite
+
+	pop XH
+	pop XL
+	pop mpr
+	ret
+
+;----------------------------------------------------------------
+; Func:		Wait
+; Desc:		A wait loop that does nothing for approximately 
+;			n*10ms where n is determined by the value stored in 
+;			waitcnt_r.
+;			The general equation for number of clock cycles 
+;			consumed by this function is as follows:
+;				(((((3*r18)-1+4)*r17)-1+4)*waitcnt)-1+16
+; Note:		No registers used by function are destroyed.
+;----------------------------------------------------------------
+wait:
+    push wait_count_r
+	push wait_inner_loop_r
+	push wait_outter_loop_r
+loop:  
+    ldi wait_outter_loop_r, 224		; load outter loop regisster
+outter_loop:  
+    ldi wait_inner_loop_r, 237		; load inner loop register
+inner_loop:  
+    dec    wait_inner_loop_r
+    brne   inner_loop				; Continue inner loop
+    dec    wait_outter_loop_r
+    brne   outter_loop				; Continue outer loop
+    dec    wait_count_r
+    brne   loop						; Continue wait loop
+
+    pop wait_outter_loop_r
+    pop wait_inner_loop_r
+    pop wait_count_r
+    ret
+
 
 ;***********************************************************
 ;*	Stored Program Data
