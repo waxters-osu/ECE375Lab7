ae375ab479a4af1f59e52d8b90a804f053abe489
IsaacGuzman2 <115694210+IsaacGuzman2@users.noreply.github.com>
2022-11-29 10:49:05 -0800

    Update Silas_Waxler_Isaac_Guzman_Lab7_SourceCode.asm

--- Silas_Waxler_Isaac_Guzman_Lab7_SourceCode.asm
+++ Silas_Waxler_Isaac_Guzman_Lab7_SourceCode.asm
@@ -9,7 +9,7 @@
 ;* 	2. Timer/counter1 Normal mode to create a 1.5-sec delay
 ;***********************************************************
 ;*
-;*	 Author: Gerardo Guzman
+;*	 Author: Silas Waxler and Isaac Guzman
 ;*	   Date: 11/16/2022
 ;*
 ;***********************************************************
