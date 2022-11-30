fbfa0c7854654b1ea20eeabe40ecaff1e71c02ba
Silas <waxters@oregonstate.edu>
2022-11-30 11:51:07 -0800

    refactor: move lcd_extended.asm into main project asm file
    Code submition to canvas requires a single file of source code.
    Therefore, `lcd_extended.asm` data is moved to
    `Isaac_Guzman_and_Silas_Waxter_Lab7_sourcecode.asm`.
    
    Note: `LCDDriver.asm` is provided by the class instruction thus it is
          not necessary to submit.

--- Isaac_Guzman_and_Silas_Waxter_Lab7_sourcecode.asm
+++ Isaac_Guzman_and_Silas_Waxter_Lab7_sourcecode.asm
@@ -52,6 +52,11 @@
 .equ button_ready_bit = 7
 .equ button_move_bit = 4
 
+.equ lcd_buffer_character_count = 15
+.equ lcd_buffer_address_start_line_1 = 0x0100
+.equ lcd_buffer_address_start_line_2 = 0x0110
+.equ lcd_buffer_address_end_line_2 = 0x011F 
+
 ;----------------------------------------------------------------
 ; Desc: debounce button wait method
 ;----------------------------------------------------------------
@@ -614,6 +619,62 @@ timer1_compare_match_A_isr:
 		pop mpr
 		ret
 
+;-----------------------------------------------------------
+; Desc:		Copies an array of n-bytes specified by param #1
+;			from program memory specified by param #2 & #3 to
+;			data memory specified by param #4 & #5.
+; Params:	These parameters should be on the stack the 
+;			following order (in order of stack push):
+;				1) length of array in bytes
+;				2) lower-byte of 16-bit program memory addresss
+;				3) upper-byte of 16-bit program memory addresss
+;				4) lower-byte of 16-bit data memory addresss
+;				5) upper-byte of 16-bit data memory addresss
+; NOTE:     Destroys contents of registers X, Y, and Z
+;-----------------------------------------------------------
+copy_prog_to_data_16:
+	; pop return address off stack temporarily to access
+	; caller's pushed data
+	pop XH
+	pop XL
+
+	; point Y at start of lcd buffer (data memory)
+	pop YH
+	pop YL
+	; point Z at start of string (program memory)
+	pop ZH
+	pop ZL
+	; left shift z because least-significant-bit toggles
+	; upper/lower byte of program memory--not apart of 
+	; program memory address.
+	; perform `<< 1` treating Z as 16 continous bits.
+	bclr 0		; clear carry-bit so that LSB will be 0 
+				; which is pointing at first byte in prog. mem.
+	rol ZL
+	rol ZH			
+
+	; pop number of bytes off stack
+	pop mpr
+
+	; push return address back onto the stack
+	push XL
+	push XH
+
+	; copy each character from program memory to buffer
+	copy_character_to_buffer:
+		; 1) move character from program memory to register 0
+		;    then point at next character in program memory.
+		lpm r0, Z+
+
+		; 2) move character from register 0 to buffer in data
+		;	 memory then point at next character in data memory
+		st Y+, r0
+
+		dec mpr
+		brne copy_character_to_buffer
+
+	ret
+
 ;----------------------------------------------------------------
 ; Desc:		A wait loop that does nothing for approximately 
 ;			n*10ms where n is determined by the value stored in 
@@ -676,5 +737,4 @@ EMPTY_STRING:
 ;***********************************************************
 ;*	Additional Program Includes
 ;***********************************************************
-.include "lcd_extended.asm"
 .include "LCDDriver.asm"
